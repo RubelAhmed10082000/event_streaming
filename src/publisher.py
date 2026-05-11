7a839a6712a95e87b1fa031d9871fe40dee6aeab
RubelAhmed10082000 <ra38697@gmail.com>
2026-05-11 18:44:30 +0100

    Adding 'publish_message' function

--- src/publisher.py
+++ src/publisher.py
@@ -2,6 +2,8 @@ from google.cloud import pubsub_v1
 from event_generator import run_event_generator
 from dotenv import load_dotenv, dotenv_values
 import os
+import json
+import time
 
 load_dotenv()
 
@@ -12,12 +14,24 @@ topic_id = os.getenv('TOPIC_ID')
 publisher = pubsub_v1.PublisherClient()
 topic_path = publisher.topic_path(project_id, topic_id)
 
-for n in range(1, 10):
-    data_str = f"Message number {n}"
+#for n in range(1, 10):
+    #data_str = f"Message number {n}"
     # Data must be a bytestring
-    data = data_str.encode("utf-8")
+    #data = data_str.encode("utf-8")
     # When you publish a message, the client returns a future.
-    future = publisher.publish(topic_path, data)
-    print(future.result())
+    #future = publisher.publish(topic_path, data)
+    #print(future.result())
 
-print(f"Published messages to {topic_path}.")
+def publish_message(topic_path):
+    while True:
+        data = run_event_generator()
+        print(data)
+        data = json.dumps(data).encode('utf-8')
+        future = publisher.publish(topic_path, data)
+        print(future.result())
+        time.sleep(1)
+
+publish_message(topic_path)
+
+    
+
