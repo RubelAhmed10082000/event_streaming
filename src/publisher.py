4c3b720103174e7cf94c0bfa1a373bd094c7e310
RubelAhmed10082000 <ra38697@gmail.com>
2026-05-13 21:33:01 +0100

    adding documentation

--- src/publisher.py
+++ src/publisher.py
@@ -1,6 +1,6 @@
 from google.cloud import pubsub_v1
 from event_generator import run_event_generator
-from dotenv import load_dotenv, dotenv_values
+from dotenv import load_dotenv
 import os
 import json
 import time
@@ -10,6 +10,17 @@ import argparse
 
 
 def publish_message(publisher, topic_path, event):
+    """
+    Publishes message to pubsub topic
+
+    Args:
+        publisher(google.cloud.pubsub_v1.PublisherClient): Identifies the publisher 
+        topic_path(str): Identifies topic that message will be published to
+        event(dict): Event that will be published 
+
+    Returns:
+        future.result(str): string objet that has been encoded 
+    """
     # Encodes data with utf-8
     data = json.dumps(event).encode('utf-8')
 
@@ -21,31 +32,47 @@ def publish_message(publisher, topic_path, event):
 
 
 def run_publisher(publisher, topic_path, rate=0):
+    """
+    Runs publish_message() function indefinetly 
+
+    Args:
+        publisher(google.cloud.pubsub_v1.PublisherClient): Identifies the publisher 
+        topic_path(str): Identifies topic that message will be published to
+        rate(int): Modifies sleep time between function calls 
+
+    """
 
     # Setting sleep rate
     sleep_time = 1 / rate
 
+    # Publish event until shutdown 
     while True:
+        # Generates event
         event =  run_event_generator()
         print(event)
 
+        # Publishes message
         message = publish_message(publisher, topic_path, event)
         print(message)
 
+        # Sleeps
         time.sleep(sleep_time)
 
 if __name__ == "__main__":
     load_dotenv()
 
+    # Create "rate" argument parser
     parser = argparse.ArgumentParser()
     parser.add_argument("--rate", type=int, default=1, help="Events per second")
     args = parser.parse_args()
 
+    # Setting up project and topic variables
     project_id = os.getenv('PROJECT_ID')
     topic_id = os.getenv('TOPIC_ID')
 
-
+    # Sets up publisher and topic path 
     pubsub_publisher = pubsub_v1.PublisherClient()
     topic_path = pubsub_publisher.topic_path(project_id, topic_id)
 
+    # Runs publisher
     run_publisher(pubsub_publisher, topic_path, rate=args.rate)
