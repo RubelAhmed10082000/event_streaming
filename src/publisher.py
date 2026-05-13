66b836684d84fd2938ff2d58bcb4cd18174cb7e5
RubelAhmed10082000 <ra38697@gmail.com>
2026-05-13 21:09:46 +0100

    Refactoring publish_message  into  run_publisher & publish_message

--- src/publisher.py
+++ src/publisher.py
@@ -16,6 +16,31 @@ topic_id = os.getenv('TOPIC_ID')
 publisher = pubsub_v1.PublisherClient()
 topic_path = publisher.topic_path(project_id, topic_id)
 
+def publish_message(publisher, topic_path, event):
+    # Encodes data with utf-8
+    data = json.dumps(event).encode('utf-8')
+
+    # Creating a 'future' object 
+    future = publisher.publish(topic_path, data)
+
+    # returning future
+    return future.result()
+
+
+def run_publisher(publisher, topic_path, rate=0):
+
+    sleep_time = 1 / rate
+
+    while True:
+        event =  run_event_generator()
+        print(event)
+
+        message = publish_message(publisher, topic_path, event)
+        print(message)
+
+        time.sleep(sleep_time)
+
+
 
 def publish_message(topic_path):
     """
