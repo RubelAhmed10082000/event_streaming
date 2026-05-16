c0e53cda1bf0043a3d2eaed3353fffc344388ca0
RubelAhmed10082000 <ra38697@gmail.com>
2026-05-16 18:57:43 +0100

    Refactoring subscriber.py

--- src/subscriber.py
+++ src/subscriber.py
@@ -1,32 +1,58 @@
 from concurrent.futures import TimeoutError
 from google.cloud import pubsub_v1
 import os
+from dotenv import load_dotenv
 
-project_id = os.getenv('PROJECT_ID')
-subscription_id = os.getenv('SUB_ID')
-
-# Number of seconds the subscriber listens for messages
-timeout = 5.0
-
-subscriber = pubsub_v1.SubscriberClient()
-
-subscription_path = subscriber.subscription_path(project_id, subscription_id)
 
 def callback(message: pubsub_v1.subscriber.message.Message) -> None:
+    """
+    Acknowledges that message has been received 
+
+    Args:
+        message(bytes): Message being listened to and acknowledged 
+    """
+    # Print statement acknowledging message
     print(f"Received {message}.")
+    # acking message
     message.ack()
 
-streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
-print(f"Listening for messages on {subscription_path}..\n")
+def future_pull(subscription_path, timeout=10):
+    """
+    Requests message from pub/sub server
 
-# Wrap subscriber in a 'with' block to automatically call close() when done.
-with subscriber:
-    try:
-        # When `timeout` is not set, result() will block indefinitely,
-        # unless an exception is encountered first.
-        streaming_pull_future.result(timeout=timeout)
-    except TimeoutError:
-        streaming_pull_future.cancel()  # Trigger the shutdown.
-        streaming_pull_future.result()  # Block until the shutdown is complete.
+    Args:
+        subscription_path (str): Path to subscriber 
+        timeout: How long subscriber listens to publisher
+    """
 
-        
+    # reads message from subscription
+    streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+    print(f"Listening for messages on {subscription_path}..\n")
+
+    # Pulls message until timeout
+    with subscriber:
+        try:
+            # If timeout not set then message pull until failure
+            streaming_pull_future.result(timeout=timeout)
+        except TimeoutError:
+            streaming_pull_future.cancel() 
+            streaming_pull_future.result()  
+
+if __name__ == "__main__":
+
+    load_dotenv()
+
+    project_id = os.getenv('PROJECT_ID')
+    subscription_id = os.getenv('SUB_ID')
+
+    subscriber = pubsub_v1.SubscriberClient()
+
+    subscription_path = subscriber.subscription_path(project_id, subscription_id)
+
+    future_pull(subscription_path, timeout=0)
+
+
+
+
+
+    
